f3ff732ce8c1af812e50950d49ceaa3d8217aa64
Satish Prasad <prasadsatish333@gmail.com>
2024-08-27 07:50:22 +0530

    rotate the array to the right by k steps.cpp

---
+++ ARRAY/rotate the array to the right by k steps.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        if (n == 0 || k == 0 || k % n == 0) {
+            return;
+        }
+
+        k = k % n;
+
+        reverse(nums.begin(), nums.end() - k);
+        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin(), nums.end());
+        
+    }
+};
